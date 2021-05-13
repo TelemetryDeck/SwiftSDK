6aeb93852969893a5fcb1811a6111331e07c3e5d
Daniel Jilg <winsmith@winsmith.de>
2021-05-13 21:35:16 +0200

    Update notice if identifierForVendor is unavailable

--- Sources/TelemetryClient/TelemetryClient.swift
+++ Sources/TelemetryClient/TelemetryClient.swift
@@ -199,7 +199,7 @@ private extension TelemetryManager {
             }
         #else
             #if DEBUG
-                print("[Telemetry] On this platform, Telemetry can't generate a unique user identifier. It is recommended you supply one yourself. More info: https://apptelemetry.io/pages/client-spec.html")
+                print("[Telemetry] On this platform, Telemetry can't generate a unique user identifier. It is recommended you supply one yourself. More info: https://apptelemetry.io/pages/signal-reference.html")
             #endif
             return "unknown user \(platform) \(systemVersion) \(buildNumber)"
         #endif
