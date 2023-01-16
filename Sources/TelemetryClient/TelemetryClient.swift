1a489fd8a6d0b046d6f3f4823a0ee26d80106c48
Daniel Jilg <daniel@telemetrydeck.com>
2023-01-16 10:41:31 +0100

    Bump Version Number to 1.3.1

--- Sources/TelemetryClient/TelemetryClient.swift
+++ Sources/TelemetryClient/TelemetryClient.swift
@@ -10,7 +10,7 @@ import Foundation
     import TVUIKit
 #endif
 
-let TelemetryClientVersion = "SwiftClient 1.3.0"
+let TelemetryClientVersion = "SwiftClient 1.3.1"
 
 public typealias TelemetrySignalType = String
 
