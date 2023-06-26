3f8bd438c8681ce7ccdf035b529cb5c4cd82cd7b
Daniel Jilg <daniel@telemetrydeck.com>
2023-06-26 14:45:33 +0200

    Update Version Number to 1.4.4

--- Sources/TelemetryClient/TelemetryClient.swift
+++ Sources/TelemetryClient/TelemetryClient.swift
@@ -10,7 +10,7 @@ import Foundation
     import TVUIKit
 #endif
 
-let TelemetryClientVersion = "SwiftClient 1.4.1"
+let TelemetryClientVersion = "SwiftClient 1.4.4"
 
 public typealias TelemetrySignalType = String
 
