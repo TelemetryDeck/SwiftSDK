52d93854f47645e57edf048492d04bad65d8db48
Daniel Jilg <daniel@telemetrydeck.com>
2024-01-11 16:39:52 +0100

    CocoaPods Support (#135)
    This adds support for CocoaPods and tries to automatically push new versions to CocoaPods whenever a new tag is created.
    
    * Update existing podspec file 
    * Add GH Action

--- Sources/TelemetryClient/TelemetryClient.h
+++ Sources/TelemetryClient/TelemetryClient.h
@@ -8,4 +8,3 @@ FOUNDATION_EXPORT const unsigned char TelemetryClientVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TelemetryClient/PublicHeader.h>
 #import <TelemetryClient/TelemetryClient.h>
-
