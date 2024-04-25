18f90562577c6bfbdd931f8f579072b696d3eae4
Cihat Gündüz <commits@cihatguenduez.de>
2024-04-26 00:11:41 +0200

    Fix tools version & provide proper support for visionOS

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
-// swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -9,7 +7,8 @@ let package = Package(
         .macOS(.v10_13),
         .iOS(.v12),
         .watchOS(.v5),
-        .tvOS(.v13)
+        .tvOS(.v13),
+        .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
