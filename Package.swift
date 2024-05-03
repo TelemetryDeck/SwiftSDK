cb3ad0cad4dc5a7732ded17022ed6cb3130f72ea
Cihat Gündüz <commits@cihatguenduez.de>
2024-05-03 19:19:24 +0200

    Add a renamed TelemetryDeck library while also keeping the old one

--- Package.swift
+++ Package.swift
@@ -11,16 +11,10 @@ let package = Package(
         .visionOS(.v1),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "TelemetryClient",
-            targets: ["TelemetryClient"]
-        ),
+        .library(name: "TelemetryDeck", targets: ["TelemetryClient"]),  // new name
+        .library(name: "TelemetryClient", targets: ["TelemetryClient"]),  // old name
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
