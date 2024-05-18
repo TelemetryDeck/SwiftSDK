3ab5db35241638736fc900237cac9991cac18a84
Cihat Gündüz <commits@cihatguenduez.de>
2024-05-18 10:28:40 +0200

    Add TelemetryDeck target to fix potential import issues

--- Package.swift
+++ Package.swift
@@ -11,13 +11,16 @@ let package = Package(
         .visionOS(.v1),
     ],
     products: [
-        .library(name: "TelemetryDeck", targets: ["TelemetryClient"]),  // new name
+        .library(name: "TelemetryDeck", targets: ["TelemetryDeck"]),  // new name
         .library(name: "TelemetryClient", targets: ["TelemetryClient"]),  // old name
     ],
     dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "TelemetryDeck",
+            dependencies: ["TelemetryClient"],
+            resources: [.copy("PrivacyInfo.xcprivacy")]
+        ),
         .target(
             name: "TelemetryClient",
             resources: [.copy("PrivacyInfo.xcprivacy")]
