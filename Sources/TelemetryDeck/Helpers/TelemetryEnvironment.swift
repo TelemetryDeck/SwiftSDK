01faf2b561990b702192aba50245c49b8e8e6e29
Mark Battistella <markbattistella@users.noreply.github.com>
2025-11-21 16:53:58 +1100

    2025-11-21 - Lint fix

--- Sources/TelemetryDeck/Helpers/TelemetryEnvironment.swift
+++ Sources/TelemetryDeck/Helpers/TelemetryEnvironment.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Represents the current telemetry execution environment.
 internal enum TelemetryEnvironment {
-    
+
     /// Indicates whether the code is running inside an app extension.
     ///
     /// Determined by checking whether the main bundle’s path ends with the `.appex` suffix.
